00093abcb440ebb699c17532b3a5a42876a5db5d
Da Wei <dawei.tang@roadshare.com>
2020-05-08 17:04:36 +0800

    add del

--- gulpfile1.js
+++ gulpfile1.js
@@ -2,7 +2,15 @@
 var gulp = require('gulp')
 var ts = require('gulp-typescript')
 var tsProject = ts.createProject('tsconfig.json')
+var del = require('del')
 
-gulp.task('default', function () {
-  return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest('dist'))
+gulp.task('clean', function () {
+  return del(['dist/*'])
 })
+
+gulp.task(
+  'default',
+  gulp.series('clean', function () {
+    return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest('dist'))
+  })
+)
